d0fad98ec0998f41e0c9b4635b963c4181a84f77
Aman Sharma <98725523+amansharma049@users.noreply.github.com>
2022-10-26 16:34:59 +0530

    Update end.js

--- end.js
+++ end.js
@@ -24,6 +24,7 @@ saveHighScore = e => {
     highScore.sort((a,b) =>  b.score - a.score);
     highScore.splice(5);
 
+    
     localStorage.setItem('highScore',JSON.stringify(highScore));
     window.location.assign('index.html'); 
 };
